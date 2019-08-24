be9c9aebd0ddc43f3496726d6902c51569a0b2c6
maryamsgithub <maryamtb.17@gmail.com>
2019-08-24 18:44:52 +0200

    back-end complete, front-end adjustments in progress

---
+++ src/app.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+require('./db/mongoose.js')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Task Manager App',
+        name: 'Task Manager'
+    })
+})
+
+app.get('/users', (req, res) => {
+    res.render('users', {
+        title: 'List of Users',
+        name: 'Users'
+    })
+})
+
+app.get('/users/login', (req, res) => {
+    if (!req.query.user) {
+        return res.send({
+            error: 'This account doesnt exist. You must create a new user.'
+        })
+
+    } res.send({
+            name: this.name,
+            email: this.email,
+            avatar: this.avatar
+        })
+})
+
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+module.exports = app
