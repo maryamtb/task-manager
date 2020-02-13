1f7fba532123dab416ee0d02e686fe5725306edb
Maryam <maryamtb.17@gmail.com>
2020-02-13 23:35:40 +0100

    jkl rm

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const app = require("./app");
 const port = process.env.PORT;
 const express = require("express");
-var ghpages = require('gh-pages');
 
 const cookieParser = require("cookie-parser");
 
@@ -14,5 +13,3 @@ app.use(cookieParser());
 app.listen(port, () => {
   console.log("Server is up on port " + port);
 });
-
-ghpages.publish('dist', callback);
